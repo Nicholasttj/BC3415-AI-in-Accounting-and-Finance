8c6a730120b66110f5c7346d2abb2d403f67cfa4
Tang Te Jie, Nicholas <156569963+Nicholasttj@users.noreply.github.com>
2025-02-13 08:21:54 +0000

    1

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():
     return render_template("index.html")
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return render_template("prediction.html")
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return render_template("dbs.html", r=(-50.6*q)+90.2)
 
 if __name__ == "__main__":
     app.run()
 
-    
